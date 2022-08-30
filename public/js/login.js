aa8ce49bd7a6fce425892a5c49686bbcf59eb6da
Eric <whodatbio@gmail.com>
2022-08-30 14:12:56 -0400

    saving before pull

--- public/js/login.js
+++ public/js/login.js
@@ -21,13 +21,14 @@ const loginFormHandler = async (event) => {
   const signupFormHandler = async (event) => {
     event.preventDefault();
   
-    const email = document.querySelector('#username-signup').value.trim();
+    const email = document.querySelector('#email-signup').value.trim();
+    const username = document.querySelector('#username-signup').value.trim();
     const password = document.querySelector('#password-signup').value.trim();
   
-    if (username && password) {
+    if (email && username && password) {
       const response = await fetch('/api/users', {
         method: 'POST',
-        body: JSON.stringify({ email, password }),
+        body: JSON.stringify({ email, username, password }),
       });
   
       if (response.ok) {
