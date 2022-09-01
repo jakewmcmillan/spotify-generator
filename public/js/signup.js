03ef24dd954986e7810ff699dffb3388def98807
natesoum <soumnathun@gmail.com>
2022-09-01 12:04:45 -0400

    Signup works and takes you to profile

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,11 +1,14 @@
+const signUpBtn = document.querySelector('#signup-btn');
+
 const signupFormHandler = async function(event) {
     event.preventDefault();
   
+ 
     const username = document.querySelector('#username-input-signup').value.trim();
     const password = document.querySelector('#password-input-signup').value.trim();
-    // console.log(username);
-    // console.log(password);
-    const response = await fetch('/api/user', {
+    console.log(username);
+    console.log(password);
+    const response = await fetch('/api/users/signup', {
       method: 'POST',
       body: JSON.stringify({
         username, password
@@ -14,12 +17,13 @@ const signupFormHandler = async function(event) {
     });
   
     if (response.ok) {
-      document.location.replace('/profile');
+      document.location.replace('/');
     } else {
       alert('Failed to sign up');
     }
   };
   
-  document
-    .querySelector('.signup-form')
-    .addEventListener('submit', signupFormHandler);
+  // document
+  //   .querySelector('.signup-form')
+  //   .addEventListener('submit', signupFormHandler);
+signUpBtn.addEventListener('click', signupFormHandler);
